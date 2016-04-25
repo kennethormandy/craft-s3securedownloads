c3c77f07cb17d07eeeffac98e1d38e06bf12e2d1
Jonathan Melville <jonathan@jonathanmelville.com>
2016-04-25 12:33:15 -0400

    Initial commit.

---
+++ README.md
@@ -0,0 +1,24 @@
+# S3 Secure Downloads plugin for Craft CMS
+
+This plugin will return a signed URL used to allow temporary S3 downloads. It also proxies the request and ensures there is a valid user session.
+
+![Screenshot](resources/screenshots/plugin_logo.png)
+
+## Installation
+
+To install S3 Secure Downloads, follow these steps:
+
+1. Download & unzip the file and place the `s3securedownloads` directory into your `craft/plugins` directory
+2.  -OR- do a `git clone ???` directly into your `craft/plugins` folder.  You can then update it with `git pull`
+3. Install plugin in the Craft Control Panel under Settings > Plugins
+4. The plugin folder should be named `s3securedownloads` for Craft to see it.  GitHub recently started appending `-master` (the branch name) to the name of the folder for zip file downloads.
+
+S3 Secure Downloads works on Craft 2.5.x.
+
+## S3 Secure Downloads Changelog
+
+### 1.0.0 -- 2016.02.25
+
+* Initial release
+
+Brought to you by [Jonathan Melville](http://jonathanmelville.com)
---
+++ services/S3SecureDownloads_UrlService.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Craft;
+
+class S3SecureDownloads_UrlService extends BaseApplicationComponent
+{
+
+	public function getSetting( $setting_name ) {
+		$plugin = craft()->plugins->getPlugin( 's3SecureDownloads' );
+		$settings = $plugin->getSettings();
+
+		return $settings->$setting_name;
+	}
+
+	public function getSignedUrl( $entry_id ) {
+
+		$entry = craft()->elements->getElementById( $entry_id );
+
+		$assetUrl = $entry->url;
+
+		$sourceType = craft()->assetSources->getSourceTypeById( $entry->sourceId );
+		$assetSettings = $sourceType->getSettings();
+
+		$urlPrefix = $assetSettings->urlPrefix;
+
+		// Remove the mtime query string just in case Craft adds it.
+		$baseAssetPath = str_replace( $urlPrefix, "", UrlHelper::stripQueryString($assetUrl) );
+
+
+		$fileName = $entry->filename;
+		$bucketName = $assetSettings->bucket;
+		$keyId = $assetSettings->keyId;
+		$secretKey = $assetSettings->secret;
+		$linkExpirationTime = $this->getSetting( "linkExpirationTime" );
+		$forceDownload = $this->getSetting( "forceFileDownload" );
+
+
+
+		$expires = time()+$linkExpirationTime;
+
+		// S3 Signed URL creation
+
+		$headers = array();
+
+		if($forceDownload) {
+			$headers["response-content-disposition"] = "attachment; filename=" . $fileName;
+		}
+
+		$resource = str_replace( array( '%2F', '%2B' ), array( '/', '+' ), rawurlencode( $baseAssetPath ) );
+
+		$string_to_sign = "GET\n\n\n$expires\n/$bucketName/$resource";
+		$final_url = "https://s3.amazonaws.com/$bucketName/$resource?";
+
+		$append_char = '?';
+		foreach ( $headers as $header => $value ) {
+			$final_url .= $header . '=' . urlencode( $value ) . '&';
+			$string_to_sign .= $append_char . $header . '=' . $value;
+			$append_char = '&';
+		}
+
+		$signature = urlencode( base64_encode( hash_hmac( 'sha1', $string_to_sign, $secretKey, true ) ) );
+
+		return $final_url . "AWSAccessKeyId=$keyId&Expires=$expires&Signature=$signature";
+
+
+	}
+}
