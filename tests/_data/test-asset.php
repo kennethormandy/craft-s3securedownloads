3db71ada61a8d8e24378367f9b13fdd583112adf
kennethormandy <hello@kennethormandy.com>
2020-01-16 12:26:11 -0800

    Updates temp test

--- tests/_data/test-asset.php
+++ tests/_data/test-asset.php
@@ -1,10 +1,12 @@
 <?php
 
+// use craft\test\fixtures\VolumeFixture; // Doesn’t exist?
+
 return [
     [
       'filename' => 'product.jpg',
       // 'tempFilePath' => 'path/to/_craft/storage/runtime/temp/product.jpg',
-      // 'volumeId' => $this->volumeIds['products'],
+      // 'volumeId' => new VolumeFixture(), 
       // 'folderId' => $this->folderIds['clothes'],
     ],
 ];
